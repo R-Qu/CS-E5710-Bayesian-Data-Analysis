307522b9e8c5f93846aa6b61bc68a5d2d88e908f
Rui_Qu <edosilence@gmail.com>
2019-12-15 15:54:39 +0200

    Add files via upload

---
+++ Assignment 09/sort.py
@@ -0,0 +1,4 @@
+
+ulist=[('Machine', 1, -31.95), ('Machine', 2, 68.0), ('Machine', 3, 12.9), ('Machine', 4, 75.95), ('Machine', 5, 19.6), ('Machine', 6, 5.65), ('Machine', 7, 21.8)]
+for l in ulist:
+    print(l) 
